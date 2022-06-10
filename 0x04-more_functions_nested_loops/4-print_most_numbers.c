72f0a688301d6c2086d7527086f552ba082cf393
kena biratu <kenabirtu19@gmail.com>
2022-06-10 05:41:47 -0700

    Do not print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers
+ *
+ * Description: a function that prints the numbers, from 0 to 9
+ *
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' &&  i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
